9281e356c16259870f0ac5380e3899876f282291
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2018-07-25 11:21:06 -0600

    added model3

--- seeds/01_models.js
+++ seeds/01_models.js
@@ -1,10 +1,11 @@
 
 exports.seed = function (knex, Promise) {
-  return knex.raw('DELETE FROM "models"; ALTER SEQUENCE models_id_seq RESTART WITH 3;')
+  return knex.raw('DELETE FROM "models"; ALTER SEQUENCE models_id_seq RESTART WITH 4;')
     .then(function () {
       return knex('models').insert([
-        { id: 1, modelLink: 'https://s3.us-east-2.amazonaws.com/capstoneportfolio/depressionModel.json' },
-        { id: 2, modelLink: 'https://s3.us-east-2.amazonaws.com/capstoneportfolio/wanderModel.json' }
+        { id: 1, modelLink: 'https://s3.us-east-2.amazonaws.com/capstoneportfolio/model0.json' },
+        { id: 2, modelLink: 'https://s3.us-east-2.amazonaws.com/capstoneportfolio/model1.json' },
+        { id: 3, modelLink: 'https://s3.us-east-2.amazonaws.com/capstoneportfolio/model2.json' }
       ]);
     });
 };
